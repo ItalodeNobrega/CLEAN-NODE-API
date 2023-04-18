496a115b7a4f06006f0318d2257e39c9f7fee003
Italo Nobrega <inobrega22@outlook.com>
2023-04-18 17:54:30 -0300

    test: ensure SignupController if no name is provider

---
+++ src/presentation/controller/signup.spec.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from './signup'
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provide', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponde = sut.handle(httpRequest)
+    expect(httpResponde.statusCode).toBe(400)
+  })
+})
