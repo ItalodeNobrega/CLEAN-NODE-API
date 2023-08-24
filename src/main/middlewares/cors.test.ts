5b27420ede07242c14ee6becf90fc2e254da50c4
Italo Nobrega <inobrega22@outlook.com>
2023-08-24 12:51:36 -0300

    feat: ensure requests return json as defaul content type

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -1,10 +1,10 @@
 import request from 'supertest'
 import app from '../config/app'
 
-describe('Cors Middleware', () => {
+describe('CORS Middleware', () => {
   test('Should enable CORS', async () => {
     app.get('/test_cors', (req, res) => {
-      res.send(req.body)
+      res.send()
     })
     await request(app)
       .get('/test_cors')
